1e51f0cb37f91899b690ac61eef90154126ac795
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-11-07 07:29:28 +0500

    update

--- app/components/Studentlist/Studentlist.tsx
+++ app/components/Studentlist/Studentlist.tsx
@@ -58,8 +58,25 @@ const studentlist = () => {
 
         }
 
+
+
         setstudentList([...students, newStudents])
 
+    }
+
+    const onchangeHandler = () => {
+        const newStudents: studenList = {
+
+            No: "09",
+            Name: "Nabeel",
+            Rollno: 223355,
+            Batch: "Mad",
+            Batchno: "01"
+
+
+        }
+
+        setstudentList([...students, newStudents])
     }
 
     return (
@@ -91,17 +108,38 @@ const studentlist = () => {
                 }
 
             </table>
-
             <button onClick={addStudent} >Add Student</button>
+
+            <br />
+            <br />
+            <br />
+            <label htmlFor="">Name</label>
+            <input type="text" placeholder="user-name" />
+            <br />
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder="user-email" />
+            <br />
+
+            <label htmlFor="">Password</label>
+            <input type="password" placeholder="password" />
+            <br />
+
+            <label htmlFor="">Roll no</label>
+            <input type="text" placeholder="user-roll no." />
+            <br />
+            <br />
+
+            <button onChange={onchangeHandler} >Add Student</button>
+
         </div>
     )
- 
+
+
+
 
 }
 
-
-
-            export default studentlist;
+export default studentlist;
 
 
 
