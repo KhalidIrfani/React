64e8983f8d595f6a05644894077238556e0cdea0
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-11-15 23:59:04 +0500

    update

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import Page from './Component/page'
+import Page from './Component/Page'
 
 export default function App({ Component, pageProps }: AppProps) {
   return( 
